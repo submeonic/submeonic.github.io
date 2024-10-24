b5d1219000f285c28056bf0e6b56f4b2d0c7dc54
Chase Franklin <chasefranklin@u.boisestate.edu>
2024-10-24 15:35:25 -0600

    Combined Project List

--- assets/js/featuredProjects.js
+++ assets/js/featuredProjects.js
@@ -23,6 +23,17 @@ const featuredProjectData = {
         tryUrl: 'https://submeonic.itch.io/distant-planets-dream-of-you-too'
     },
     'project3': {
+        title: 'Sounds of the Depths',
+        subtitle: 'Ability Platformer | Fall 2023',
+        videoUrl: 'https://www.youtube-nocookie.com/embed/QPgFnWwPFME?modestbranding=1&rel=0&iv_load_policy=3&controls=1',
+        roles: 'Gameplay Designer, Programmer, Artist, SFX Designer, Dynamic Audio Designer',
+        stack: 'Unity, Photoshop, Ableton Live, PlusMusic.Ai, C#',
+        overview: "Created as a playable demonstration of PlusMusic's Unity plugin. Intended to showcase adaptive sound tracking. Also designed functional UI display for PlusMusic plugin status and control. Corresponded with PlusMusic Head of Marketing to nail down design requirements.",
+        contributors: 'Individual',
+        repositoryUrl: '',
+        tryUrl: ''
+    },
+    'project4': {
         title: 'Temporal Tides',
         subtitle: 'Paradox Naval Combat | Fall 2023',
         videoUrl: 'https://www.youtube-nocookie.com/embed/XR885q3O0XQ?modestbranding=1&rel=0&iv_load_policy=3&controls=1',
@@ -33,7 +44,18 @@ const featuredProjectData = {
         repositoryUrl: '',
         tryUrl: 'https://papadaplus.itch.io/temporal-tides'
     },
-    'project4': {
+    'project5': {
+        title: 'Visualization of 3D Point Clouds in VR',
+        subtitle: 'Rendering Technique Research | Under Review for IEEE VR 2024',
+        videoUrl: 'https://www.youtube-nocookie.com/embed/u4NaMHy6hY4?modestbranding=1&rel=0&iv_load_policy=3&controls=1',
+        roles: 'Primary Author, Sole Developer, Data Set Constructor',
+        stack: 'Unreal Engine, Niagara Particle System, NeRF Studio, CloudCompare, Meshlab, C++, UE Blueprints, Python',
+        overview: "A specialized pipeline that optimizes the rendering of point clouds on standalone HMDs. We use a particle system usually used for high-performance particle effects to perform the rendering of the points. In this pipeline, the point cloud---including positions, colors, and normals---is encoded into textures that the HMD GPU can directly read. The particle system uses this data to instantiate particles that represent each point in the cloud, rendering them efficiently in the VR environment. As a result, we can render large and detailed point cloud datasets on standalone HMDs without sacrificing performance or visual fidelity. Our implementation uses the Unreal Engine's Niagara particle system but can be easily implemented in any similar particle system.",
+        contributors: 'Translational AI Center (TrAC), Adarsh Krishnamurthy, Shambhavi Joshi',
+        pdf: '',
+        repositoryUrl: 'https://github.com/idealab-isu/VRPointCloud'
+    },
+    'project6': {
         title: 'Dead West',
         subtitle: 'Interactive Animation | Spring 2023',
         videoUrl: 'https://www.youtube-nocookie.com/embed/10OWIMlVvq0?modestbranding=1&rel=0&iv_load_policy=3&controls=1',
@@ -41,6 +63,17 @@ const featuredProjectData = {
         stack: 'Animate, Photoshop, Unity, Ableton Live, C#, OpenCV, Python',
         overview: 'Dead West is a short animation with a focus on building atmosphere and visual storytelling. The project also features a computer vision gazed control system that I developed, to allow the user to control their viewing angle with their head',
         contributors: 'Carson Strout',
+        repositoryUrl: '',
+        tryUrl: ''
+    },
+    'project7': {
+        title: 'Plus Music Graphic Video',
+        subtitle: 'Animated Graphics | Summer 2023',
+        videoUrl: 'https://www.youtube-nocookie.com/embed/4CqYB9phfDY?modestbranding=1&rel=0&iv_load_policy=3&controls=1',
+        roles: 'Animator, Graphic Designer',
+        stack: 'After Effects, Photoshop',
+        overview: "Animated a graphic video demonstrating PlusMusic's use of AI in their platform. Corresponded with PlusMusic CEO to nail down design requirements over multiple itterations.",
+        contributors: 'Individual',
         repositoryUrl: '',
         tryUrl: ''
     },
